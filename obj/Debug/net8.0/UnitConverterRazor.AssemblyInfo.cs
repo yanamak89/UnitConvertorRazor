1db05946ddb3a1c097eaa598b08fa8fc8ebeec39
Yana Makogon <yana.mak89@gmail.com>
2025-12-04 21:11:05 +0100

    Add Dockerfile for Render deployment

--- obj/Debug/net8.0/UnitConverterRazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/UnitConverterRazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConverterRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c101b74469aaf51ddd60a9294d3d221b12369046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f08c49f3914bebf879f10dbc71dc08cd6934256")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConverterRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConverterRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
